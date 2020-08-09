88db3f5897d2f4fd638156064ea13ec4b8535813
hasemi <h171754@gmail.com>
2020-08-09 23:29:05 +0900

    Remove 주석 삭제

--- backend/src/main/java/com/example/socket/Application.java
+++ backend/src/main/java/com/example/socket/Application.java
@@ -3,9 +3,6 @@ package com.example.socket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * 스프링 애플리케이션 구동
- */
 @SpringBootApplication
 public class Application {
 
