467f4d589eff26ecba730950ec3402128e10b96c
hasemi <h171754@gmail.com>
2020-08-09 23:20:47 +0900

    테스트

--- backend/src/main/java/com/example/socket/Application.java
+++ backend/src/main/java/com/example/socket/Application.java
@@ -3,6 +3,9 @@ package com.example.socket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 스프링 애플리케이션 구동
+ */
 @SpringBootApplication
 public class Application {
 
