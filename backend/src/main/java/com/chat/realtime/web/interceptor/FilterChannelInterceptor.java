cb89c11a2105919465ef5a6d10d9bcfd665c6baa
hasemi <h171754@gmail.com>
2020-09-09 07:00:04 +0900

    [20200909] 공통 에러 처리 및 인증 수정

---
+++ backend/src/main/java/com/chat/realtime/web/interceptor/FilterChannelInterceptor.java
@@ -0,0 +1,59 @@
+package com.chat.realtime.web.interceptor;
+
+import com.chat.realtime.web.exception.CommonException;
+import com.chat.realtime.web.util.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.util.MultiValueMap;
+
+import java.util.List;
+
+@Slf4j
+public class FilterChannelInterceptor implements ChannelInterceptor {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        MessageHeaders headers = message.getHeaders();
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
+
+        log.info("full message:" + message);
+
+        if (StompCommand.CONNECT.equals(headerAccessor.getCommand())) {
+            log.info("CONNECT ==================================");
+            log.info("auth:" + headerAccessor.getNativeHeader("Authorization"));
+            MultiValueMap<String, String> multiValueMap = headers.get(StompHeaderAccessor.NATIVE_HEADERS, MultiValueMap.class);
+
+            List<String> list = (List<String>) multiValueMap.get("Authorization");
+            String token = list.get(0);
+
+            // TODO: 2020-09-09 개발 임시 삭제 예정
+            if ("SUPER_TOKEN".equals(token)) {
+                return message;
+            }
+
+            if (!jwtUtil.isValidToken(token)) {
+                throw new CommonException(401, "유효한 토큰이 아닙니다.");
+            }
+
+        } else if (StompCommand.SUBSCRIBE.equals(headerAccessor.getCommand())) {
+            log.info("SUBSCRIBE ==================================");
+        } else if (StompCommand.SEND.equals(headerAccessor.getCommand())) {
+            log.info("SEND ==================================");
+        } else if (StompCommand.DISCONNECT.equals(headerAccessor.getCommand())) {
+            log.info("DISCONNECT ==================================");
+        }
+
+
+        return message;
+    }
+}
